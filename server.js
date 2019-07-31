bea0a02616008efc602608f1417ba8e24950aee6
Gil Fewster <hello@gilfewster.com>
2019-07-31 13:52:06 +1000

    moved server port into config.json file, and change port number to 3000

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const fs = require('fs');
+const configFile = 'config.json';
 
-server.listen(80);
+const init = async configFile => {
+  const config = await loadConfig(configFile);
 
-app.get('/', function(req, res) {
-  res.sendFile(`${__dirname}/index.html`);
-});
+  // const { server, devices } = config;
+  // const boseClient = getConnection(devices[0].ip, devices[0].protocol);
+  // boseClient.on('message', function incoming(data) {
+  //   sendMessage(data);
+  // });
 
-io.on('connection', function(socket) {
-  socket.emit('service', { event: 'connection accepted' });
-  socket.on('ping', function(data) {
-    console.log(`ping received: ${data}`);
+  createServer(config.server);
+
+  app.get('/', function(req, res) {
+    res.sendFile(`${__dirname}/index.html`);
   });
-});
+
+  io.on('connection', function(socket) {
+    socket.emit('service', { event: 'connection accepted' });
+    socket.on('ping', function(data) {
+      console.log(`ping received: ${data}`);
+    });
+  });
+};
+
+const loadConfig = configPath => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(configPath, (err, data) => {
+      if (err) reject(err);
+      resolve(JSON.parse(data));
+    });
+  });
+};
+
+const createServer = ({ port }) => {
+  server.listen(port, () => {
+    console.log('I am listening');
+  });
+};
+
+init(configFile);
 
 /*
 
@@ -27,24 +56,6 @@ wss.on('connection', ws => {
 });
 
 server.listen(8081);
-
-const init = async () => {
-  const config = await loadConfig('config.json');
-  const { server, devices } = config;
-  const boseClient = getConnection(devices[0].ip, devices[0].protocol);
-  boseClient.on('message', function incoming(data) {
-    sendMessage(data);
-  });
-};
-
-const loadConfig = configPath => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(configPath, (err, data) => {
-      if (err) reject(err);
-      resolve(JSON.parse(data));
-    });
-  });
-};
 
 const getConnection = (ip, protocol) => {
   const conn = new WebSocket(`ws://${ip}:8080`, protocol);
@@ -60,6 +71,4 @@ const sendMessage = data => {
     }
   });
 };
-
-init(configFile);
 */
